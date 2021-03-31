3c48da0eb86aec686fa9e5b8a63a8c2d7a59924c
Manishriya1 <75018171+Manishriya1@users.noreply.github.com>
2021-03-31 12:49:17 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -24,7 +24,7 @@ plot1<- function(){
         ## subset data from 2007-02-01 and 2007-02-02
         subsetdata <- subset(powerdata, Date == "2007-02-01" | Date =="2007-02-02")
         
-        ## plot histogram of global active power for those 2 days
+        ###plot histogram of global active power for those 2 days
         png("plot1.png", width=480, height=480)
         hist(subsetdata$Global_active_power, col="red", border="black", main ="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency")
         dev.off()
